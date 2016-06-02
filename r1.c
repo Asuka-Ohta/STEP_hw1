fd75d5ec0ee57ef2b653b4c85745534ded870d5c
Asuka-Ohta <ringochan0815@gmail.com>
2016-06-02 17:06:07 +0900

    Upload r1.c

--- r1.c
+++ r1.c
@@ -16,8 +16,8 @@ int compare(char *a, char *b){
 int main(){
   FILE *fp,*fq;
   char data1[16]; //受け取った16文字
-  char data2[WORD_LENGTH]; //辞書単語の文字列
-  char data3[WORD_LENGTH]; //sortした辞書単語の文字列
+  char *data2; //辞書単語の文字列
+  char *data3; //sortした辞書単語の文字列
   char *ch;
   int k=0;
   int *x;
@@ -25,29 +25,25 @@ int main(){
   printf("please input 16 characters : \n");
   scanf("%s",data1);
   qsort(data1, sizeof(data1)-1, sizeof(data1[0]), compare);
-
   
-  /*  前処理(辞書を文字数の大きい順に並び替える、16文字以上の単語は排除)
   if((fp = fopen("/usr/share/dict/words", "r")) == NULL){
     printf("We can not open the file.\n");
     exit(EXIT_FAILURE);
   }
   fq  = fopen("sorted_dict.txt", "w");
+  
+  /*  前処理(辞書を文字数の大きい順に並び替える、16文字以上の単語は排除)
   while (fgets(ch, WORD_LENGT, fp) != NULL){
     x[k] = strlen(ch);
     for(int l=0;l<k;l++)
       
     fprintf(fq,"%s\n",ch); 
-  } 
-  */
+    } */
 
-  if((fp = fopen("dict2.txt", "r")) == NULL){
-    printf("We can not open the file.\n");
-    exit(EXIT_FAILURE);
-  }
-  
   while (fgets(data2, WORD_LENGTH, fq) != NULL){
     int i=0,j=0;
+    data2 = (char*)malloc(WORD_LENGTH);
+    data3 = (char*)malloc(WORD_LENGTH);
     strcpy(data3,data2);
     qsort(data3, sizeof(data3)-1, sizeof(data3[0]), compare);
     
@@ -58,6 +54,7 @@ int main(){
       }else
 	i++;
     }
+    
     if(i<16)
       printf("The longest word : %s\n",data2);
     return 0;
